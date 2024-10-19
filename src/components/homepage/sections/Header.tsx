ba9d3fa0db3acb064a35f4b38ad9f09d789ba1de
Francesco Velluto <vellfr00@gmail.com>
2024-10-19 17:35:47 +0200

    Implemented language switch and integrated with header

--- src/components/homepage/sections/Header.tsx
+++ src/components/homepage/sections/Header.tsx
@@ -1,11 +1,13 @@
 import Typewriter, { TypewriterClass } from 'typewriter-effect';
 import { useTranslation } from 'react-i18next';
+import { useEffect, useState } from 'react';
 
 interface HeaderProps {
   onAnimationFinished: () => void;
+  isLanguageChanged?: boolean;
 }
 
-function Header({ onAnimationFinished }: HeaderProps) {
+function Header({ onAnimationFinished, isLanguageChanged }: HeaderProps) {
   const HOMEPAGE_HEADER_CLASSNAME = 'homepage-header';
   const NAME_TYPEWRITER_H1_ID = 'name-typewriter';
   const PROFESSION_TYPEWRITER_H2_ID = 'profession-typewriter';
@@ -17,7 +19,7 @@ function Header({ onAnimationFinished }: HeaderProps) {
   let greetingsTypewriter: TypewriterClass | null = null;
   let discoverMoreTypewriter: TypewriterClass | null = null;
 
-  const { t } = useTranslation('Homepage');
+  const { t } = useTranslation('pages/Homepage');
 
   const setCursorDisplayByContainerId = (containerId: string, display: string): void => {
     const cursor = document.querySelector<HTMLElement>(`#${containerId} .Typewriter__cursor`);
@@ -25,88 +27,101 @@ function Header({ onAnimationFinished }: HeaderProps) {
       cursor.style.display = display;
   }
 
+  /**
+  * When language changes, animation will already be completed but strings are not replaced.
+  * Detecting language change will substitute typewriters with simple text.
+  * By doing this, the correct translation will be displayed.
+  * */
   return (
     <div className={HOMEPAGE_HEADER_CLASSNAME}>
       <h1 id={NAME_TYPEWRITER_H1_ID}>
-        <Typewriter
-          onInit={(_nameTypewriter) => {
-            _nameTypewriter
-              .typeString(t('Header.NAME'))
-              .callFunction(() => {
-                professionTypewriter?.start();
-                setCursorDisplayByContainerId(NAME_TYPEWRITER_H1_ID, 'none');
-              })
-              .stop();
+        { isLanguageChanged ? t('Header.NAME') : 
+          <Typewriter
+            onInit={(_nameTypewriter) => {
+              _nameTypewriter
+                .typeString(t('Header.NAME'))
+                .callFunction(() => {
+                  professionTypewriter?.start();
+                  setCursorDisplayByContainerId(NAME_TYPEWRITER_H1_ID, 'none');
+                })
+                .stop();
 
-            nameTypewriter = _nameTypewriter;
-          }}
-        />
+              nameTypewriter = _nameTypewriter;
+            }}
+          />
+        }
       </h1>
       <h2 id={PROFESSION_TYPEWRITER_H2_ID}>
-        <Typewriter
-          onInit={(_professionTypewriter) => {
-            setCursorDisplayByContainerId(PROFESSION_TYPEWRITER_H2_ID, 'none');
+        { isLanguageChanged ? t('Header.PROFESSION') : 
+          <Typewriter
+            onInit={(_professionTypewriter) => {
+              setCursorDisplayByContainerId(PROFESSION_TYPEWRITER_H2_ID, 'none');
 
-            _professionTypewriter
-              .callFunction(() => {
-                setCursorDisplayByContainerId(PROFESSION_TYPEWRITER_H2_ID, 'inline');
-              })
-              .changeDelay(50)
-              .pauseFor(500)
-              .typeString(t('Header.PROFESSION'))
-              .callFunction(() => {
-                greetingsTypewriter?.start();
-                setCursorDisplayByContainerId(PROFESSION_TYPEWRITER_H2_ID, 'none');
-              })
-              .stop();
+              _professionTypewriter
+                .callFunction(() => {
+                  setCursorDisplayByContainerId(PROFESSION_TYPEWRITER_H2_ID, 'inline');
+                })
+                .changeDelay(50)
+                .pauseFor(500)
+                .typeString(t('Header.PROFESSION'))
+                .callFunction(() => {
+                  greetingsTypewriter?.start();
+                  setCursorDisplayByContainerId(PROFESSION_TYPEWRITER_H2_ID, 'none');
+                })
+                .stop();
 
-            professionTypewriter = _professionTypewriter;
-          }}
-        />
+              professionTypewriter = _professionTypewriter;
+            }}
+          />
+        }
       </h2>
       <h3 id={GREETINGS_TYPEWRITER_H3_ID}>
-        <Typewriter
-          onInit={(_greetingsTypewriter) => {
-            setCursorDisplayByContainerId(GREETINGS_TYPEWRITER_H3_ID, 'none');
+        { isLanguageChanged ? t('Header.GREETINGS') : 
+          <Typewriter
+            onInit={(_greetingsTypewriter) => {
+              setCursorDisplayByContainerId(GREETINGS_TYPEWRITER_H3_ID, 'none');
 
-            _greetingsTypewriter
-              .callFunction(() => {
-                setCursorDisplayByContainerId(GREETINGS_TYPEWRITER_H3_ID, 'inline');
-              })
-              .changeDelay(50)
-              .pauseFor(300)
-              .typeString(t('Header.GREETINGS'))
-              .callFunction(() => {
-                discoverMoreTypewriter?.start();
-                setCursorDisplayByContainerId(GREETINGS_TYPEWRITER_H3_ID, 'none');
-              })
-              .stop();
+              _greetingsTypewriter
+                .callFunction(() => {
+                  setCursorDisplayByContainerId(GREETINGS_TYPEWRITER_H3_ID, 'inline');
+                })
+                .changeDelay(50)
+                .pauseFor(300)
+                .typeString(t('Header.GREETINGS'))
+                .callFunction(() => {
+                  discoverMoreTypewriter?.start();
+                  setCursorDisplayByContainerId(GREETINGS_TYPEWRITER_H3_ID, 'none');
+                })
+                .stop();
 
-            greetingsTypewriter = _greetingsTypewriter;
-          }}
-        />
+              greetingsTypewriter = _greetingsTypewriter;
+            }}
+          />
+        }
       </h3>
       <span id={DISCOVER_MORE_TYPEWRITER_SPAN_ID}>
-        <Typewriter
-          onInit={(_discoverMoreTypewriter) => {
-            setCursorDisplayByContainerId(DISCOVER_MORE_TYPEWRITER_SPAN_ID, 'none');
+        { isLanguageChanged ? t('Header.DISCOVER_MORE') : 
+          <Typewriter
+            onInit={(_discoverMoreTypewriter) => {
+              setCursorDisplayByContainerId(DISCOVER_MORE_TYPEWRITER_SPAN_ID, 'none');
 
-            _discoverMoreTypewriter
-              .callFunction(() => {
-                setCursorDisplayByContainerId(DISCOVER_MORE_TYPEWRITER_SPAN_ID, 'inline');
-              })
-              .changeDelay(50)
-              .pauseFor(300)
-              .typeString(t('Header.DISCOVER_MORE'))
-              .callFunction(() => setCursorDisplayByContainerId(DISCOVER_MORE_TYPEWRITER_SPAN_ID, 'none'))
-              .stop()
-              .callFunction(() => onAnimationFinished());
+              _discoverMoreTypewriter
+                .callFunction(() => {
+                  setCursorDisplayByContainerId(DISCOVER_MORE_TYPEWRITER_SPAN_ID, 'inline');
+                })
+                .changeDelay(25)
+                .pauseFor(300)
+                .typeString(t('Header.DISCOVER_MORE'))
+                .callFunction(() => setCursorDisplayByContainerId(DISCOVER_MORE_TYPEWRITER_SPAN_ID, 'none'))
+                .stop()
+                .callFunction(() => onAnimationFinished());
 
-            discoverMoreTypewriter = _discoverMoreTypewriter;
+              discoverMoreTypewriter = _discoverMoreTypewriter;
 
-            nameTypewriter?.start();
-          }}
-        />
+              nameTypewriter?.start();
+            }}
+          />
+        }
       </span>
     </div>
   )
--- src/pages/Homepage.tsx
+++ src/pages/Homepage.tsx
@@ -3,19 +3,24 @@ import Header from '../components/homepage/sections/Header';
 import MegaScroll from "react-mega-scroll";
 import { useState } from 'react';
 import ScrollableSection from '../components/homepage/ScrollableSection';
+import FloatingLanguageSwitch from '../components/homepage/FloatingLanguageSwitch';
 
 function Homepage() {
   const [isHeaderAnimationFinished, setIsHeaderAnimationFinished] = useState(false);
-  const [activeSectionIndex, setActiveSectionIndex] = useState(0);
+  const [isLanguageChanged, setIsLanguageChanged] = useState(false);
 
   return (
-    <MegaScroll onChange={ setActiveSectionIndex }>
-      <ScrollableSection>
-        <Header 
-          onAnimationFinished = { () => setIsHeaderAnimationFinished(true) }
-        />
-      </ScrollableSection>
-    </MegaScroll>
+    <>
+      <MegaScroll>
+        <ScrollableSection>
+          <Header 
+            onAnimationFinished = { () => setIsHeaderAnimationFinished(true) }
+            isLanguageChanged = { isLanguageChanged }
+          />
+        </ScrollableSection>
+      </MegaScroll>
+      { isHeaderAnimationFinished && <FloatingLanguageSwitch onLanguageChange={ () => setIsLanguageChanged(true) } /> }
+    </>
   )
 }
 
