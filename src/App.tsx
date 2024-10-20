4bd35e71fd0d3da07a0303129217ecdab208f6f4
Francesco Velluto <vellfr00@gmail.com>
2024-10-20 11:33:15 +0200

    Update HTML lang attribute dinamically on language change

--- src/App.tsx
+++ src/App.tsx
@@ -3,8 +3,19 @@ import Homepage from './pages/Homepage';
 
 import './styles/common/colors.css';
 import './styles/common/scrollbar.css';
+import { useEffect } from 'react';
+import { useTranslation } from 'react-i18next';
 
 function App() {
+  const { i18n } = useTranslation();
+
+  /**
+   * Update the HTML lang attribute to the current language dynamically.
+   */
+  useEffect(() => {
+    document.documentElement.lang = i18n.language;
+  }, [i18n.language]);
+
   return (
     <BrowserRouter>
       <Routes>
