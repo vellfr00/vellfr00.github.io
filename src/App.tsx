08c0e3dc02798c751011d29f3e31f845289187f0
Francesco Velluto <vellfr00@gmail.com>
2024-11-01 09:45:57 +0100

    Enable scroll in pages different than homepage

--- src/App.tsx
+++ src/App.tsx
@@ -10,6 +10,8 @@ import { useTranslation } from 'react-i18next';
 import Contacts from './pages/Contacts';
 
 function App() {
+  const APP_UNSET_OVERFLOW_CLASSANAME = 'unset-overflow';
+
   const { i18n } = useTranslation();
 
   /**
@@ -18,6 +20,21 @@ function App() {
   useEffect(() => {
     document.documentElement.lang = i18n.language;
   }, [i18n.language]);
+
+  /**
+   * When we are not in the homepage, enable scroll which is disabled in the homepage
+   * from the react-mega-scroll library with overflow hidden CSS property.
+   * */
+  useEffect(() => {
+    if(window.location.pathname !== '/') {
+      document.body.classList.add(APP_UNSET_OVERFLOW_CLASSANAME);
+      document.getElementById('root')?.classList.add(APP_UNSET_OVERFLOW_CLASSANAME);
+    }
+    else {
+      document.body.classList.remove(APP_UNSET_OVERFLOW_CLASSANAME);
+      document.getElementById('root')?.classList.remove(APP_UNSET_OVERFLOW_CLASSANAME);
+    }
+  }, []);
 
   return (
     <BrowserRouter>
